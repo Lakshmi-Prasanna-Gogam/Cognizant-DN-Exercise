4ea00368bf8734dc192907b6210be7e91079ec21
Lakshmi Prasanna Gogam <lakshmiprasanna@gmail.com>
2026-06-29 21:45:35 +0530

    Spring and core

---
+++ Week-2/SpringCore and Maven/Dependency Injection/demo/src/main/java/com/library/LibraryApplication.java
@@ -0,0 +1,21 @@
+package com.library;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.library.service.BookService;
+
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml")) {
+
+            BookService service = context.getBean("bookService", BookService.class);
+
+            service.addBook();
+
+        }
+
+    }
+
+}
---
+++ Week-2/SpringCore and Maven/Dependency Injection/demo/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,13 @@
+package com.library.repository;
+
+public class BookRepository {
+
+    public BookRepository() {
+        System.out.println("BookRepository object created.");
+    }
+
+    public void saveBook() {
+        System.out.println("Book saved successfully.");
+    }
+
+}
